6c4337b101503883a80df88b01c82389caa66836
davydenko919 <davydenko919@gmail.com>
2024-09-12 15:05:30 +0300

    sssssssss

--- src/components/movie-api.js
+++ src/components/movie-api.js
@@ -1,20 +1,20 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-const options = {
-  method: 'GET',
-  url: 'https://api.themoviedb.org/3/trending/movie/day',
-  params: {language: 'en-US'},
-  headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YzA0MThiZGRmNjQ5MTkwOGFlOGQ3NjAyNWIxYTBlZiIsIm5iZiI6MTcyNjE0MjAxMy4xODk3OCwic3ViIjoiNjZlMmQ0NzI5MDEzZmU4NzIyMjM4OTZjIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.n-854o4ROi7GQ_orn4sTy7_ChpuAHK_Z7vCL86fumPY'
-  }
-};
+// const options = {
+//   method: 'GET',
+//   url: 'https://api.themoviedb.org/3/trending/movie/day',
+//   params: {language: 'en-US'},
+//   headers: {
+//     accept: 'application/json',
+//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YzA0MThiZGRmNjQ5MTkwOGFlOGQ3NjAyNWIxYTBlZiIsIm5iZiI6MTcyNjE0MjAxMy4xODk3OCwic3ViIjoiNjZlMmQ0NzI5MDEzZmU4NzIyMjM4OTZjIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.n-854o4ROi7GQ_orn4sTy7_ChpuAHK_Z7vCL86fumPY'
+//   }
+// };
 
-axios
-  .request(options)
-  .then(function (response) {
-    console.log(response.data);
-  })
-  .catch(function (error) {
-    console.error(error);
-  });
+// axios
+//   .request(options)
+//   .then(function (response) {
+//     console.log(response.data);
+//   })
+//   .catch(function (error) {
+//     console.error(error);
+//   });
