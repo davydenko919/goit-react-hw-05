b17e8396bb1a2f6f5c68bbc862174098af7b66f3
davydenko919 <davydenko919@gmail.com>
2024-09-12 15:44:47 +0300

    dasfdawfwa

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route, NavLink  } from "react-router-dom";
-import MoviesPage from "./pages/MoviesPage/MoviesPage";
-import MovieDetailsPage from "./pages/MovieDetailsPage/MovieDetailsPage";
-import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+import MoviesPage from "../pages/MoviesPage/MoviesPage";
+import MovieDetailsPage from "../pages/MovieDetailsPage/MovieDetailsPage";
+import NotFoundPage from "../pages/NotFoundPage/NotFoundPage";
 import clsx from 'clsx';
 import css from './App.module.css';
 // import { lazy } from "react";
@@ -21,7 +21,8 @@ function App() {
           {/* <NavLink to="/" className={buildLinkClass}>
             Home
           </NavLink> */}
-          <NavLink to="/movies" className={buildLinkClass}>
+          {/* <NavLink to="/movies" className={buildLinkClass}> */}
+          <NavLink to="/" className={buildLinkClass}>
             About
           </NavLink>
           <NavLink to="/movies/:movieId" className={buildLinkClass}>
